cf097b5e1cb7cdcc91af0b7f1745ceb631d22d17
takashi <tiranotirano888@gmail.com>
2025-04-22 00:38:25 +0900

    chore: バージョンを0.3.2に更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crules",
-    version="0.1.0",
+    version="0.3.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
